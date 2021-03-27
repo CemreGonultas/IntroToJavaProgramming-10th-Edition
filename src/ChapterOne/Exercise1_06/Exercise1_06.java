18d4122075ce5e5856471bacd359b7439441cd7d
CemreGonultas <cemre.gonultas@hotmail.com>
2021-03-27 23:38:30 +0300

    Create Exercise1_06.java

---
+++ src/ChapterOne/Exercise1_06/Exercise1_06.java
@@ -0,0 +1,10 @@
+package ChapterOne.Exercise1_06;
+/*
+(Summation of a series) Write a program that displays the result of
+ */
+
+public class Exercise1_06 {
+    public static void main(String[] args) {
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    }
+}
