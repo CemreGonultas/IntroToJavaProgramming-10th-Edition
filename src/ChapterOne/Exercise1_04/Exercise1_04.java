24b3d693022c7cb76fb609e5ce20031ffb9c366c
CemreGonultas <cemre.gonultas@hotmail.com>
2021-03-27 23:38:18 +0300

    Create Exercise1_04.java

---
+++ src/ChapterOne/Exercise1_04/Exercise1_04.java
@@ -0,0 +1,14 @@
+package ChapterOne.Exercise1_04;
+/*
+(Print a table) Write a program that displays the following table:
+ */
+public class Exercise1_04 {
+    public static void main(String[] args) {
+        System.out.println("a     a^2    a^3\n" +
+                    "1      1      1\n" +
+                    "2      4      8\n" +
+                    "3      9      27\n" +
+                    "4      16     64");
+
+    }
+}
