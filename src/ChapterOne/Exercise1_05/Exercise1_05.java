fd3c460130fabd558a10157e0a90766ff6eb1aea
CemreGonultas <cemre.gonultas@hotmail.com>
2021-03-27 23:38:24 +0300

    Create Exercise1_05.java

---
+++ src/ChapterOne/Exercise1_05/Exercise1_05.java
@@ -0,0 +1,10 @@
+package ChapterOne.Exercise1_05;
+/*
+(Compute expressions) Write a program that displays the result of
+ */
+
+public class Exercise1_05 {
+    public static void main(String[] args) {
+        System.out.println(((9.5*4.5)-(2.5*3))/(45.5-3.5));
+    }
+}
