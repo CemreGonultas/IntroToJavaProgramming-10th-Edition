6be4d4ac92899082bf6dcf381d54b55bc39b848a
CemreGonultas <cemre.gonultas@hotmail.com>
2021-03-27 23:38:13 +0300

    Create Exercise1_03.java

---
+++ src/ChapterOne/Exercise1_03/Exercise1_03.java
@@ -0,0 +1,13 @@
+package ChapterOne.Exercise1_03;
+/*(Display a pattern) Write a program that displays the following pattern:
+*/
+public class Exercise1_03 {
+    public static void main(String[] args) {
+        System.out.println("     J    A  V     V  A\n" +
+                    "     J   A A  V   V  A A\n" +
+                    "J    J  AAAAA  V V  AAAAA\n" +
+                    " J J   A     A  V  A     A");
+
+    }
+}
+
