d747ae00212e2a2447b2afc56b7343fb21b2cfac
CemreGonultas <cemre.gonultas@hotmail.com>
2021-03-29 00:36:33 +0300

    Create Exercise3_03.java

---
+++ src/ChapterThree/Exercise3_03/Exercise3_03.java
@@ -0,0 +1,55 @@
+package ChapterThree.Exercise3_03;
+/*
+(Random month) Write a program that randomly generates an integer between 1
+and 12 and displays the English month name January, February, …, December for
+the number 1, 2, …, 12, accordingly.
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise3_03 {
+    public static void main(String[] args) {
+        int number = (int) ((Math.random() * 12) + 1);
+        System.out.println("The random number: " + number);
+        switch (number){
+            case 1 :
+                System.out.println("January");
+                break;
+            case 2 :
+                System.out.println("February");
+                break;
+            case 3 :
+                System.out.println("March");
+                break;
+            case 4 :
+                System.out.println("April");
+                break;
+            case 5 :
+                System.out.println("May");
+                break;
+            case 6 :
+                System.out.println("June");
+                break;
+            case 7 :
+                System.out.println("July");
+                break;
+            case 8 :
+                System.out.println("August");
+                break;
+            case 9 :
+                System.out.println("September");
+                break;
+            case 10 :
+                System.out.println("October");
+                break;
+            case 11 :
+                System.out.println("November");
+                break;
+            case 12 :
+                System.out.println("December");
+                break;
+
+        }
+    }
+}
